eb29b7d00566f131a08a147b366ba8b038e1dd73
Abhinav Gorantla <56104674+AbhinavGor@users.noreply.github.com>
2019-10-10 21:04:00 +0530

    Updated minor changes for the output type,etc.

--- Percentage HRR.py
+++ Percentage HRR.py
@@ -3,7 +3,7 @@ students_public = list()
 student = list()
 student_str = list()
 
-num = int(input("How many students do you wan to include?"))
+num = int(input(""))
 
 i = 0
 
@@ -13,9 +13,9 @@ while i < num:
     student = student_str.split(" ")
     students_public.append(student)
 
-print(students_public)
+#print(students_public)
 
-percentage_stud = str(input("Enter the student's name whose percentage you want."))
+percentage_stud = str(input(""))
 
 total = 0
 j = 1
@@ -24,12 +24,13 @@ for i in range(len(students_public)):
     test = percentage_stud == students_public[i][0]
     if test:
         for j in range(1,len(students_public[i])):
-            total  = total + int(students_public[i][j])
+            total  = total + float(students_public[i][j])
             percentage =total/3
 
-for i in students_public:
-    for j in i:
-        print(j, end = "\t")
-    print("\n")
+#for i in students_public:
+ #   for j in i:
+  #      print(j, end = "\t")
+   # print("\r")
 
-print(percentage)
+print("%.2f" %percentage,end = "")
+
