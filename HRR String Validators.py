c2882442bd5743f66b525f3dbed50e4801dca9f0
Abhinav Gorantla <56104674+AbhinavGor@users.noreply.github.com>
2019-10-13 18:42:34 +0530

    String Validators

---
+++ HRR String Validators.py
@@ -0,0 +1,40 @@
+s = input()
+
+alpha = 0
+digit = 0
+lower = 0
+upper = 0
+
+
+if s.isalnum():
+    print("True")
+    if s.isalpha:
+        print(s.isalpha())
+        print("if ALpha")
+        print(s.isdigit())
+        print("if dalpha")
+
+    if s.isdigit():
+        print(s.isalpha())
+        print("ifadigit")
+        print(s.isdigit())
+        print("didigit")
+    if not(s.isalpha()):
+        print(not(s.isalpha()))
+        print("not al")
+    if not(s.isdigit()):
+        print(not(s.isdigit()))
+        print("not dig")
+
+
+for i in s:
+    if i.islower():
+        print("True LOW")
+        break;
+
+for i in s:
+    if i.isupper():
+        print("True UP")
+        break;
+
+
